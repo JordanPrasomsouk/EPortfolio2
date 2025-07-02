416f962cd0590c8d7a87ddb9a94232d9c8eeac25
JordanPrasomsouk <jordan.prasomsouk@outlook.com>
2025-07-02 23:56:12 +1000

    Done Modal, dark theme and contact me animation

---
+++ app.js
@@ -0,0 +1,47 @@
+let isModalOpen = false;
+let contrastToggle = false;
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+        document.body.classList += " dark-theme"
+    }
+    else {
+        document.body.classList.remove("dark-theme");
+    }
+}
+
+
+
+function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
+    loading.classList += " modal__overlay--visible";
+
+    emailjs
+        .sendForm(
+            'service_hw7ytta',
+            'template_ka7fgub',
+            event.target,
+            'EFlIGxzJE79XtnglU'
+        ).then(() =>{
+            loading.classList.remove("modal__overlay--visible");
+            success.classList += " modal__overlay--visible";
+        }).catch(() =>{
+            loading.classList.remove("modal__overlay--visible");
+            alert(
+                "The email service is temporarily unavailable. Please contact me directly on Jordan.Prasomsou@gmail.com."
+            );
+        })
+}
+
+function toggleModal() {
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove("modal__open");
+    }
+    isModalOpen = true;
+    //Toggle Modal
+    document.body.classList += " modal__open"
+}
